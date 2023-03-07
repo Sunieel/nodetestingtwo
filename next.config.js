184c3aeb3f290b6a97975ead4c605e800529f608
Sunieel <50975264+Sunieel@users.noreply.github.com>
2023-03-07 07:51:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images.unoptimized = true
+}
+
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
